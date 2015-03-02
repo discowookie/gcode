fb504cfa920c1ab8fdfbf61e6e3f1c96efdb85df
John Looney <john.looney@gmail.com>
2015-03-01 20:10:38 -0800

    Move the laser 20 times over the one spot

--- simple_dither.py
+++ simple_dither.py
@@ -6,6 +6,7 @@ import random
 length=50
 width=40
 step=0.2
+burn_count=10
   
 # reset
 print("g0 x-50 y-50 ( reset axis )")
@@ -21,8 +22,9 @@ for row in range(0, int(length/step)):
     if(r <= int(row)):
       print "g0 x%.1f y%.1f" % (row*step, col*step) 
       print "M3"
-      print "g1 x%.1f y%.1f" % (row*step, (col+1)*step) # move it over one step
-      print "g1 x%.1f y%.1f" % (row*step, col*step)     # and back
+      for time in range (0, burn_count):
+	print "g1 x%.1f y%.1f" % (row*step, (col+1)*step) # move it over one step
+	print "g1 x%.1f y%.1f" % (row*step, col*step)     # and back
       print "g1 x%.1f y%.1f" % (row*step, (col+1)*step) # and back over a third time
       print "M5"
       dots += 1
