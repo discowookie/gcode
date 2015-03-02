798c5ca3aaedf9a25479e1ac4c1b5bd15df6ffdc
John Looney <john.looney@gmail.com>
2015-03-01 19:20:31 -0800

    Simple dither program, using random to draw a number of dots on a line, then move on to the next one.
    It doesn't adjust for light loss along the plastic yet

---
+++ simple_dither.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python 
+
+import random
+
+# defs
+length=50
+width=40
+step=0.2
+  
+# reset
+print("g0 x-50 y-50 ; reset axis")
+print("g92 x0 y0 ; set current position to x=0,y=0")
+
+for row in range(0, int(length/step)):
+  # 250 rows
+  random.seed()
+  dots = 0
+  for col in range(0, int(width/step)):
+    # 200 cols
+    r = random.randint(0, length/step)
+    if(r <= int(row)):
+      print "g1 x%.1f y%.1f" % (row*step, col*step) 	# Don't bother dithering the first bit
+      print "M3\nM5"  		# do we need delays here ?
+      dots += 1
+  print "; printed %d dots, going to new line" % dots
+
