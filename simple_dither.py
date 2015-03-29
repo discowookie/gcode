f8853631cc55a7e4bb5a711d7bc901169ed639e0
codykung <cody@Bantu.local>
2015-03-29 14:15:02 -0700

    Use dwell command instead of seeking
    Laser Cube supports the dwell command (g4), so use that instead of
    seeking back and forth.
    
    Rename overstep to padding, and clarify comments a bit.
    
    Remove per-dot comment, since it’s now easy to see just be scanning the
    commands.

--- simple_dither.py
+++ simple_dither.py
@@ -2,25 +2,25 @@
 
 import random
 
+# laser bed geometry
 xmax = 40
 ymax = 40
 quantum = 0.2
 
-# number of oscillations when etching a single point
-# TODO(cody): investigate using g4 (dwell) instead
-burn_count = 10
+# length of time to lase a single dot
+dwell = 0.5
 
 # maximum distance between mechanical limit and working limit
-overstep = 10
+padding = 10
 
 # deterministic pro tempore
 random.seed(0)
 
 # reset laser 
-print "(resetting laser position)"
+print "(prepare laser)"
 print "m5 (turn off laser)"
-print "g0 x-%d y-%d (reset axis)" % (xmax + overstep, ymax + overstep)
-print "g92 x0 y0 (set current position to zero)"
+print "g0 x-%d y-%d (move to home) " % (xmax + padding, ymax + padding)
+print "g92 x0 y0 (reset axes)"
 
 for xstep in range(0, int(xmax / quantum)):
   x = xstep * quantum
@@ -29,12 +29,9 @@ for xstep in range(0, int(xmax / quantum)):
     y = ystep * quantum
     yd = y + quantum
     if random.randint(0, xmax / quantum) <= xstep:
-      print "(printing dot at (%.1f, %.1f))" % (x, y)
       print "g0 x%.1f y%.1f" % (x, y)
       print "m3"
-      for time in range (0, burn_count):
-        print "g1 x%.1f y%.1f" % (x, yd) # move it over one quantum
-        print "g1 x%.1f y%.1f" % (x, y)  # and back
+      print "g4 p%.1f" % dwell
       print "m5"
       dots += 1
   print "(printed %d dots on row %d)" % (dots, xstep)
