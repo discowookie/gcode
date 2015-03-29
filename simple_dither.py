193fe9c7030b7e04267f1fbaca4da49e035f211b
codykung <cody@Bantu.local>
2015-03-29 13:09:58 -0700

    Various minor changes
    - Turn off laser at start of reset sequence
    - Add padding in x axis to avoid mechanical limit
    - Add more detailed comments
    - Switch from rows and columns to x and y axes
    - Use a fixed seed for RNG
    - Remove one unnecessary step per etched dot
    - General Python style cleanup

--- simple_dither.py
+++ simple_dither.py
@@ -2,31 +2,40 @@
 
 import random
 
-# defs
-length=50
-width=40
-step=0.2
-burn_count=10
-  
-# reset
-print("g0 x-50 y-50 ( reset axis )")
-print("g92 x0 y0 ( set current position to x=0,y=0 )")
+xmax = 40
+ymax = 40
+quantum = 0.2
 
-for row in range(0, int(length/step)):
-  # 250 rows
-  random.seed()
+# number of oscillations when etching a single point
+# TODO(cody): investigate using g4 (dwell) instead
+burn_count = 10
+
+# maximum distance between mechanical limit and working limit
+overstep = 10
+
+# deterministic pro tempore
+random.seed(0)
+
+# reset laser 
+print "(resetting laser position)"
+print "m5 (turn off laser)"
+print "g0 x-%d y-%d (reset axis)" % (xmax + overstep, ymax + overstep)
+print "g92 x0 y0 (set current position to zero)"
+
+for xstep in range(0, int(xmax / quantum)):
+  x = xstep * quantum
   dots = 0
-  for col in range(0, int(width/step)):
-    # 200 cols
-    r = random.randint(0, length/step)
-    if(r <= int(row)):
-      print "g0 x%.1f y%.1f" % (row*step, col*step) 
-      print "M3"
+  for ystep in range(0, int(ymax / quantum)):
+    y = ystep * quantum
+    yd = y + quantum
+    if random.randint(0, xmax / quantum) <= xstep:
+      print "(printing dot at (%.1f, %.1f))" % (x, y)
+      print "g0 x%.1f y%.1f" % (x, y)
+      print "m3"
       for time in range (0, burn_count):
-	print "g1 x%.1f y%.1f" % (row*step, (col+1)*step) # move it over one step
-	print "g1 x%.1f y%.1f" % (row*step, col*step)     # and back
-      print "g1 x%.1f y%.1f" % (row*step, (col+1)*step) # and back over a third time
-      print "M5"
+        print "g1 x%.1f y%.1f" % (x, yd) # move it over one quantum
+        print "g1 x%.1f y%.1f" % (x, y)  # and back
+      print "m5"
       dots += 1
-  print "( printed %d dots, going to new line )" % dots
+  print "(printed %d dots on row %d)" % (dots, xstep)
 
